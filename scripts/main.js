345dc6b4d4c013d1479c3504adfc8e036444a76a
juligarc91 <julissagarcia@pursuit.org>
2023-11-02 15:25:05 -0400

    Connected html form to JS application

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+// ------ ATTACHING JAVASCRIPT TO HTML ------
+// grabbing form
 const form = document.querySelector("form");
+
+// getting event listener "submit" - Appends an event listener for events whose type attribute value is type "submit" in this case. The callback argument sets the callback that will be invoked when the event is dispatched
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // the default is reloading page whenever submit button is clicked
+    console.log ("I have submitted my form");
+});
