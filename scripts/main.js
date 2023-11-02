504ee98d66e48cef4476703d010d442f5d069418
juligarc91 <julissagarcia@pursuit.org>
2023-11-02 15:51:31 -0400

    Added event listener

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,8 @@ const form = document.querySelector("form");
 // getting event listener "submit" - Appends an event listener for events whose type attribute value is type "submit" in this case. The callback argument sets the callback that will be invoked when the event is dispatched
 form.addEventListener("submit", (event) => {
     event.preventDefault(); // the default is reloading page whenever submit button is clicked
-    console.log ("I have submitted my form");
+    // console.log ("I have submitted my form"); // this is shown in DOM
+    console.log ("Plant:", event.target.plant.value);// to grab values of the form make an event target and add .value to get the values inputted in fields instead of the html code. Logs to DOM console
+    console.log ("Color:", event.target.color.value);
+    console.log ("Description:", event.target.notes.value);
 });
